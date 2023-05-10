11658645198771cb72b5ec26655f72e1c6bc4e0b
arushi <arushig749@gmail.com>
2023-05-10 17:52:24 +0530

    API calls for updating and deleting data

--- server.js
+++ server.js
@@ -2,31 +2,16 @@ const express =require('express')
 const { MongoClient,ObjectId } = require('mongodb');
 const bodyParser=require('body-parser');
 const cors=require('cors');
-const bcrypt = require('bcrypt');
-// const fileUpload = require('express-fileupload');
-
 const {connectToDb, getDb}=require('./db')
+const xlsx = require('xlsx');
 
-// const api = require('./routes/api')
 const app = express()
 
 const PORT=3000
 const dbName='Apexdb';
 const connectionString=new MongoClient('mongodb://localhost:27017/' + dbName);
 const uri='mongodb://localhost:27017/'+dbName;
-// const Schema =mongoose.Schema;
 
-// const advocateSchema=new Schema({
-//     advocateName:String,
-//     mobileNumber:number,
-//     joiningDate:String,
-//     address:String,
-// })
-
-//  MongoClient.connect(connectionString);
-
-// const AdvocateModel=mongoose.model('advocates',advocateSchema);
-// app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -55,10 +40,11 @@ connectToDb((err)=>{
 let AdvocateCollection;
 let ClientCollection;
 let NewCaseCollection;
+let LoginCredentialsCollection;
 
 function init() {
     db=getDb();
-    LoginCredentialsCollection= db.collection('LoginCredentials');
+    LoginCredentialsCollection= db.collection('CredentialsCollection');
     AdvocateCollection = db.collection('advocates');
     ClientCollection   = db.collection('Clients');
     NewCaseCollection  = db.collection('NewCases');
@@ -93,7 +79,7 @@ app.get('/cases',async(req,res)=>{
 app.post('/advocates',function(req,res){
     console.log(req.body)
     AdvocateCollection.insertOne(req.body);
-    res.json("Advocate added successfully")
+    res.json({"status":"success","message":"Advocate added successfully"})
 
 
  })
@@ -102,7 +88,7 @@ app.post('/advocates',function(req,res){
 app.post('/clients',function(req,res){
     console.log(req.body)
     ClientCollection.insertOne(req.body);
-    res.json({"status":"success","message":"Client added successfully"})
+    res.json({"status":"success","message":"Client added successfully"});
 })
 
 
@@ -110,135 +96,96 @@ app.post('/clients',function(req,res){
 app.post('/cases',function(req,res){
     console.log(req.body)
     NewCaseCollection.insertOne(req.body);
-    res.json({'status':'success','message':"New Case added successfully"})
+    res.json({"status":"success","message":"New Case added successfully"})
 })
 
 
 // API for authentication
 
-const saltRounds = 10;
-
-app.get('/login-credentials', (req, res) => {
+app.post('/login-credentials', async(req, res) => {
   console.log(req.body);
-  console.log(LoginCredentialsCollection);
-    LoginCredentialsCollection.find({ "email": req.body.email }, function(err, user) {
+  console.log("Received request:", req.body); // Debug statement
+  try{
+    console.log("Email and password:", req.body.email, req.body.password); // Debug statement
+
+    const{email,password}=req.body;
+    
+    // validating the request body 
+    if(!email ||!password){
+       return res.status(400).json({ message: 'Email and password are required' });
+    }
+    // it is working till now 
+    // check if uer exists in the database 
+    //const user =await LoginCredentialsCollection.findOne({email});
+    
+    LoginCredentialsCollection.find({ email: req.body.email },  function(err, user) {
       console.log(err);
-      if (user) {
-        if(user.password===user.password && user.email===req.body.email){
-            res.status(200).json({'login':'success'})
-        }else{
-          res.status(401).json({'login':'failed','message':'inavlid username or password'});
+
+      if (user.length>0) {
+        if(user[0].password === req.body.password && user[0].email === req.body.email){
+            res.status(200).json({login:'success'})
+        } else {
+          res.status(401).json({login:'failed','message':'invalid username or password'});
         }
       } else {
-        res.status(401).send({'login':'failed','message':'email id not registered'});
+        res.status(401).send({login:'failed','message':'email id not registered'});
       }
     });
+  }catch(error){
+    console.log(error);
+    return res.status(500).json({message:"caught internal server error"}); 
+  }
+  
 });
 
 
-// app.post('/LoginCredentials', async (req, res) => {
-//     const { username, password } = req.body;
-  
-//     try {
-//       // Connect to the MongoDB database
-//       const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
-//       const db = client.db();
-  
-//       // Find the user with the provided username
-//       const user = await db.collection('LoginCredentials').findOne({ username });
-  
-//       if (!user) {
-//         return res.status(400).json({ message: 'Invalid credentials' });
-//       }
-  
-//       // Compare the provided password with the hashed password stored in the user document
-//       const isMatch = await bcrypt.compare(password, user.password);
-  
-//       if (!isMatch) {
-//         return res.status(400).json({ message: 'Invalid credentials' });
-//       }
-  
-//       // If credentials are valid, you can send a success response or create a JWT token
-//       return res.status(200).json({ message: 'Login successful' });
-  
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ message: 'Server error' });
-//     } finally {
-//       // Close the MongoDB database connection
-//       await client.close();
-//     }
-//   });
   
 // API for deleting data
 
   app.delete('/advocates/:id', async (req, res) => {
-    const id = req.params.id;
-  
+    const id = decodeURI(req.params.id);
+
     try {
-      // Connect to the MongoDB database
-      // const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
-      // const db = client.db();
-  
-      // Delete the document with the provided id
-      const result = await AdvocateCollection.deleteOne({ _id: ObjectId(id) });
-  
+      const result = await AdvocateCollection.deleteOne({ _id: id });
       if (result.deletedCount === 0) {
-        return res.status(404).json({ message: 'Data not found' });
+        return res.status(404).json({ message: 'Advocate Details not found' });
       }
-  
       // If delete operation is successful, send a success response
-      return res.status(200).json({ message: 'Data deleted successfully' });
+      return res.status(200).json({ message: 'Advocate Details deleted successfully' });
   
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Server error' });
      }
-    // finally {
-    //   // Close the MongoDB database connection
-    //   await client.close();
-    // }
+    
+  });
+
+
+  //  API for downloading data in excel sheet 
+
+  app.get('/advocates', async (req, res) => {
+    const dataType = req.params.dataType;
+  
+    // get the data from the database based on the data type
+
+    // ...
+    console.log(req.body)
+    const adArray = await AdvocateCollection.find({}).toArray();
+    res.json(adArray);
+  
+    // convert the data into an Excel file
+    const worksheet = xlsx.utils.json_to_sheet(data);
+    const workbook = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+  
+    // set the response headers and send the file to the frontend
+    res.setHeader('Content-Disposition', `attachment; filename=${adArray}.xlsx`);
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.send(excelBuffer);
   });
 
 
 
 
-
-
-
-
-
-
-
-
-
-// Define GET route to retrieve selected element
-
-// app.get('/advocates', (req, res) => {
-//     const selectedElementId = req.query.selectedElementId;
-//     AdvocateCollection.findById(selectedElementId, (err, adv) => {
-//       if (err) {
-//         console.log(err);
-//         return res.status(500).send(err);
-//       }
-//       if (!adv) {
-//         return res.status(404).send('Advocate not found');
-//       }
-//       res.status(200).send(adv);
-//     });
-//   });
-// // Defined DELETE route to delete element by ID
-// app.delete('/advocates/:id', (req, res) => {
-//     const id = req.params.id;
-//     AdvocateCollection.''(id, (err, adv) => {
-//       if (err) {
-//         console.log(err);
-//         return res.status(500).send(err);
-//       }
-//       if (!adv) {
-//         return res.status(404).send('Advocate not found');
-//       }
-//       res.status(200).send(`Advocate ${adv._name} with id ${adv._id} deleted`);
-//     });
-//   });
 
